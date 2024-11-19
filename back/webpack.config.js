f70e3a9b801bbdd221e72860b80970ad8593cd89
김승섭 <b24019.tdc.hanmacgroup@gmail.com>
2024-11-19 17:58:21 +0900

    webpack 설정 변경

--- back/webpack.config.js
+++ back/webpack.config.js
@@ -7,4 +7,25 @@ module.exports = {
     filename: 'main.js',
   },
   mode: 'development', // 필요에 따라 설정
+  resolve: {
+    fallback: {
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "url": require.resolve("url/"),
+      "crypto": require.resolve("crypto-browserify"),
+      "assert": require.resolve("assert/"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "path": require.resolve("path-browserify"),
+      "fs": false, // 브라우저 환경에서는 `fs`를 사용할 수 없으므로 false로 설정
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
 };
