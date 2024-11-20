38944385a21754a3ec34f76f0f7849198a5cbce9
김승섭 <sub9707@naver.com>
2024-11-20 20:10:49 +0900

    PORT removed

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `https://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}`, // 서버의 기본 URL로 설정합니다.
+  baseURL: `https://${import.meta.env.VITE_API_HOST}`, // 서버의 기본 URL로 설정합니다.
   timeout: 30000, // 요청 타임아웃을 설정할 수 있습니다. (단위: ms)
   headers: {
     'Content-Type': 'application/json',
--- front/src/components/common/Auth/KakaoLogin.jsx
+++ front/src/components/common/Auth/KakaoLogin.jsx
@@ -5,7 +5,7 @@ import kakaoButtonWide from '/src/assets/icons/kakao_login_medium_wide.png';
 
 function KakaoLogin({wide=false}) {
     const Rest_api_key = import.meta.env.VITE_KAKAO_REST_KEY; // REST API KEY
-    const redirect_uri = `https://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}/auth/kakao/callback`; // Backend endpoint for handling Kakao's response
+    const redirect_uri = `https://${import.meta.env.VITE_API_HOST}/auth/kakao/callback`; // Backend endpoint for handling Kakao's response
 
     // OAuth 요청 URL
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
