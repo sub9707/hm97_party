d979190507ccc0ace139d30cd5ff3aa74572fd38
김승섭 <sub9707@naver.com>
2025-01-02 02:17:19 +0900

    menu routes 수정

--- front/src/components/mainPage/MainMenuArea.jsx
+++ front/src/components/mainPage/MainMenuArea.jsx
@@ -11,8 +11,7 @@ function MainMenuArea() {
     <div className={styles.AreaWrapper}>
         <MainMenu title={'갤러리'} route={'/gallery'} text={'이제껏 모은 사진들을 모아 갤러리에 어쩌고'} image={testImg} />
         <MainMenu title={'담벼락'} route={'/comments'} text={'하고싶은 말들 다~털어보자아!'} image={testImg}/>
-        <MainMenu title={'롤링페이퍼'} text={'내게 온 롤링페이퍼와 내가 쓰고 싶은 한마디씩'} image={testImg}/>
-        <MainMenu title={'롤링페이퍼'} text={'내게 온 롤링페이퍼와 내가 쓰고 싶은 한마디씩'} image={testImg}/>
+        <MainMenu title={'롤링페이퍼'} route={'/rollingPaper'} text={'내게 온 롤링페이퍼와 내가 쓰고 싶은 한마디씩'} image={testImg}/>
     </div>
   )
 }
