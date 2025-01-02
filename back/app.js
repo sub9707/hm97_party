b8437372087133a7cf64369611249e364803b50f
김승섭 <sub9707@naver.com>
2025-01-02 21:44:57 +0900

    rollbacked

--- back/app.js
+++ back/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
@@ -9,31 +10,15 @@ const galleryRoutes = require('./router/GalleryRoute');
 const commentRoutes = require('./router/CommentsRoute');
 const rollingPaperRoutes = require('./router/rollingPaperRoute');
 
-const app = express();
+app.use(express.json({
+    limit : "50mb"
+}));
+app.use(express.urlencoded({
+    limit:"50mb",
+    extended: false
+}));
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: false }));
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        const allowedOrigins = [
-            process.env.FRONTEND_URL_DEV,
-            process.env.FRONTEND_URL_PRODUCTION,
-        ];
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // 쿠키 포함 요청 허용
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Preflight 요청 처리
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/db', dbRoutes);
@@ -42,8 +27,9 @@ app.use('/gallery', galleryRoutes);
 app.use('/comment', commentRoutes);
 app.use('/rolling', rollingPaperRoutes);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
+const host = process.env.ENVIRONMENT == 'DEVELOPMENT' ? process.env.API_URL_DEV : process.env.API_URL_PRODUCTION;
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+app.listen(port, host, () => {
+    console.log(`Server started on http://${host}:${port}`);
 });
