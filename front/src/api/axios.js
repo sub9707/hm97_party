3f1cbd0fde2aa59c43cacf379973f778206a2ab9
김승섭 <sub9707@naver.com>
2025-01-02 18:30:59 +0900

    axios 주소 수정

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
+// 환경 변수에 따라 baseURL 설정
+const baseURL =
+  import.meta.env.VITE_ENVIRONMENT === 'DEVELOPMENT'
+    ? `http://${import.meta.env.VITE_API_HOST_DEVELOPMENT}:${import.meta.env.VITE_API_PORT}`
+    : `https://${import.meta.env.VITE_API_HOST_PRODUCTION}`;
+
+// Axios 인스턴스 생성
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_ENVIRONMENT === 'DEVELOPMENT' ? `http://${import.meta.env.VITE_API_HOST_DEVELOPMENT}:${import.meta.env.VITE_API_PORT}` : `https://${import.meta.env.VITE_API_HOST_PRODUCTION}`,
-  timeout: 30000, // 요청 타임아웃을 설정할 수 있습니다. (단위: ms)
+  baseURL, // API 기본 URL
+  timeout: 30000, // 요청 타임아웃 (단위: ms)
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json', // 기본 Content-Type
   },
+  withCredentials: true, // 쿠키 및 인증 정보 포함
 });
 
 export default axiosInstance;
