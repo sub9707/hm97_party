33c0805860728a413a88268cd0a0802180d6f596
김승섭 <sub9707@naver.com>
2025-01-02 21:24:39 +0900

    axios 수정

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,21 +1,11 @@
 import axios from 'axios';
 
-// 환경 변수에 따라 baseURL 설정
-const baseURL =
-  import.meta.env.VITE_ENVIRONMENT === 'DEVELOPMENT'
-    ? `http://${import.meta.env.VITE_API_HOST_DEVELOPMENT}:${import.meta.env.VITE_API_PORT}`
-    : `https://${import.meta.env.VITE_API_HOST_PRODUCTION}`;
-
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_ENVIRONMENT === 'DEVELOPMENT'
-    ? `http://${import.meta.env.VITE_API_HOST_DEVELOPMENT}:${import.meta.env.VITE_API_PORT}`
-    : `https://${import.meta.env.VITE_API_HOST_PRODUCTION}`,
-  timeout: 30000,
+  baseURL: import.meta.env.VITE_ENVIRONMENT === 'DEVELOPMENT' ? `http://${import.meta.env.VITE_API_HOST_DEVELOPMENT}:${import.meta.env.VITE_API_PORT}` : `https://${import.meta.env.VITE_API_HOST_PRODUCTION}`,
+  timeout: 30000, // 요청 타임아웃을 설정할 수 있습니다. (단위: ms)
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true, // 쿠키 포함 요청
 });
-
 
 export default axiosInstance;
